6c8082afcfe38bbc15385e3492e21567969fa325
yeemans <arthur.roth.07@gmail.com>
2020-11-12 10:46:55 -0800

    made neater

--- caesar.rb
+++ caesar.rb
@@ -3,24 +3,19 @@ def cipher(text, offset)
     ascii = []
     i = 0
     while i < text.length do
-        if text[i].ord >= "a"[0].ord && text[i].ord <= "z"[0].ord ||
-        text[i].ord >= "A"[0].ord && text[i].ord <= "Z"[0].ord 
-            ascii.push(text[i].ord + offset)
-            #for lowercase letters
-            if text[i].ord + offset > "z"[0].ord 
-                ascii[i] -= 26
-            end
-            #for uppercase letters
-            if text[i].ord + offset > "Z"[0].ord && text[i].ord + offset < "a"[0].ord
-                ascii[i] -= 26
-            end
-
-            ascii[i] = ascii[i].chr
-        
-        else 
-            ascii.push(text[i])
-        end
-        i = i + 1
+      if text[i].ord >= "a"[0].ord && text[i].ord <= "z"[0].ord ||
+      text[i].ord >= "A"[0].ord && text[i].ord <= "Z"[0].ord 
+        ascii.push(text[i].ord + offset)
+        # for lowercase letters
+        ascii[i] -= 26 if text[i].ord + offset > "z"[0].ord 
+        # for uppercase letters
+        ascii[i] -= 26 if text[i].ord + offset > "Z"[0].ord && 
+        text[i].ord + offset < "a"[0].ord
+        ascii[i] = ascii[i].chr
+      else 
+        ascii.push(text[i])
+      end
+      i += 1
     end
     ascii = ascii.join("")
     puts(ascii)
@@ -29,5 +24,5 @@ end
 
 cipher("HELLO",13)
 cipher("hELLO", 1)
-cipher("world", 13)
+cipher("Hello world", 3)
 cipher("Be sure to drink your Ovaltine", 13)
